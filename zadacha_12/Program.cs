504cc664dab07c2a9fbcb3061be5f8e61b56a3db
Larina <Losiknor@gmail.com>
2022-02-07 14:45:46 +0700

    Задача 12

---
+++ zadacha_12/Program.cs
@@ -0,0 +1,4 @@
+﻿// № 12 Удалить вторую цифру трёхзначного числа
+int numberA = new Random().Next(100, 999);
+Console.WriteLine("Дано число   " + numberA);
+Console.WriteLine("Число без второй цифры   " + (numberA / 100) + "_" + (numberA%10));
