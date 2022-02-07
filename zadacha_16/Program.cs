8e4b44198013e98bbded734cef1a591ee9b2908e
Larina <Losiknor@gmail.com>
2022-02-08 02:32:39 +0700

    Задача 16

---
+++ zadacha_16/Program.cs
@@ -0,0 +1,7 @@
+﻿// № 16 Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+int numberA = new Random().Next(1, 7);
+Console.WriteLine("Дано число   " + numberA);
+if ((numberA == 6)||(numberA == 7) )
+Console.WriteLine("Это выходной");
+else
+Console.WriteLine("Это не выходной");
