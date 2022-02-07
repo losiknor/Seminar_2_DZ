e887ed8ed8d5a1d51dac1e89210c8438079b4208
Larina <Losiknor@gmail.com>
2022-02-08 02:17:42 +0700

    Задача 14

---
+++ zadacha_14/Program.cs
@@ -0,0 +1,8 @@
+﻿// № 14 Найти третью цифру числа или сообщить, что её нет
+int numberA = new Random().Next(1, 10000);
+Console.WriteLine("Дано число   " + numberA);
+int[] arr = numberA.ToString().ToCharArray().Select(x => x - '0').ToArray();
+if (arr.Length < 3)
+Console.WriteLine("Третьей цифры нет");
+else
+Console.WriteLine("Третья цифра   " + arr[2]);
