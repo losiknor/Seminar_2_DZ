935e6a8fc3ef8194eb59d19ca42e9a3f901eacb9
Larina <Losiknor@gmail.com>
2022-02-07 14:33:48 +0700

    Задача 10

---
+++ zadacha_10/Program.cs
@@ -0,0 +1,4 @@
+﻿// № 10 Показать вторую цифру трёхзначного числа
+int numberA = new Random().Next(100, 999);
+Console.WriteLine("Дано число   " + numberA);
+Console.WriteLine("Вторая цифра   " + ((numberA % 100) / 10));
